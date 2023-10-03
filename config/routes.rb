381e3d9cd1ee0666e339b5364c860141aca9d85f
Vincent Ollivier <v@vinc.cc>
2023-10-03 08:52:26 +0200

    Add homepage on main domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   post "/join", to: "subscribers#create"
+  constraints domain: "wasting.cloud" do
+    get "/", to: "pages#landing"
+  end
   root "pages#home"
 end
