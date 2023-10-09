6e92fd4e16c50cce2535e59a41ea254c0e19db72
Vincent Ollivier <v@vinc.cc>
2023-10-09 08:15:57 +0200

    Add canonical tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def landing
+    set_meta_tags canonical: "https://stop.wasting.cloud/"
   end
 
   def home
+    set_meta_tags canonical: "https://cloudwaste.net/"
   end
 end
